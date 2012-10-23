69d070478e1e712c418bbdaf0607e49747120697
Lucas <lucas@lovethebomb.eu>
2012-10-23 09:16:44 +0200

    Initial Commit

---
+++ TextEditor/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TextEditor
+//
+//  Created by HEYMES Lucas on 23/10/12.
+//  Copyright (c) 2012 Heymès Lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TextEditor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TextEditor
+//
+//  Created by HEYMES Lucas on 23/10/12.
+//  Copyright (c) 2012 Heymès Lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
