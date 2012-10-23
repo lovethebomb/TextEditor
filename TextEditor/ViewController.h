09b46acad173879087c32f9e9ff6d232f8700ea4
Lucas <lucas@lovethebomb.eu>
2012-10-23 16:07:37 +0200

    Check input text

--- TextEditor/ViewController.h
+++ TextEditor/ViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate, UITextViewDelegate>
 
 @property (retain, nonatomic) IBOutlet UITextField *searchField;
 @property (retain, nonatomic) IBOutlet UITextView *textView;
--- TextEditor/ViewController.m
+++ TextEditor/ViewController.m
@@ -57,6 +57,53 @@
 }
 - (IBAction)doneButtonPressed:(id)sender {
     // removes the keyboard when clicking editable textview
-    [textView resignFirstResponder];
+    [searchField resignFirstResponder];
+    doneButton.hidden = YES;
 }
+
+#pragma mark - TextField delegate method
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)theTextField
+{
+    doneButton.hidden = NO;
+    return YES;
+}
+
+#pragma mark - TextField delegate method
+- (BOOL) textFieldShouldReturn:(UITextField *)theTextField
+{
+    [searchField resignFirstResponder];
+    doneButton.hidden = YES;
+    NSLog(@"searchField text : %@", theTextField.text);
+    // ignore if field is empty
+    if (![searchField.text isEqualToString:@""]) {
+        
+        
+        NSRange foundRange = [textView.text rangeOfString:theTextField.text];
+        if (foundRange.location != NSNotFound) {
+            [textView becomeFirstResponder];
+            textView.selectedRange = foundRange;
+        }
+        else {
+            [searchField becomeFirstResponder];
+            searchField.text = @"";
+        } 
+        
+    }
+    return NO;
+}
+
+#pragma mark - TextView delegate method - accept only numbers
+- (BOOL) textView:textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    // This is what I type
+    NSLog(@"%@ = %d", text, [text intValue]);
+    
+    // We test if integer value of text is either not 0 (int), OR if it's 0 (NSString)oi
+    if ( ([text intValue] != 0) || ([text isEqualToString:@"0"]) ) {
+        // text is a number
+        return YES;
+    }
+    return NO;
+}
+
 @end
